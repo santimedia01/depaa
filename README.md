b4c13f3fe0ece59eb6d5c1fbe684277ad0750810
depaa <lautarodepascali@gmail.com>
2020-09-30 20:00:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bot-modular-discordjs-v12
+> Agregar el token de su Bot en `config.js`
+# depaa
---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+ dirBase : './database/base_1.db',
+ superusers: [], //Puedes añadir ID
+ token: 'Njc1MDc5MDYzOTAzMTQxODk5.Xjx6Xw.dWtEA2R1UFJIEYa996635ort4xw',
+ prefix: '.d',
+ statusBOT: 'Estoy listo!',
+ categories: [
+  {name: "general", priority: 5},
+  {name: "admin", priority: 8}
+ ],
+ groups: [
+  {name: "User", permlvl: 0},
+  {name: "Member", permlvl: 1},
+  {name: "Mod", permlvl: 2},
+  {name: "Admin", permlvl: 3}
+ ]
+ 
+}
